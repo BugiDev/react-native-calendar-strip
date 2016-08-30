eaa252afb3e2e178646fb3fdd83899acd159b2c2
Bogdan Begovic <bogdanbegovic@gmail.com>
2016-08-30 10:39:21 +0200

    Added android example

--- example/ReactNativeCalendarStripExample/index.android.js
+++ example/ReactNativeCalendarStripExample/index.android.js
@@ -4,50 +4,36 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    View
 } from 'react-native';
 
-class ReactNativeCalendarStripExample extends Component {
+import CalendarStrip from 'react-native-calendar-strip';
+
+class ReactNativeCalendarStrip extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View>
+          <CalendarStrip
+              calendarAnimation={{type: 'sequence', duration: 30}}
+              selection={'border'}
+              selectionAnimation={{duration: 300, borderWidth: 2}}
+              style={{paddingTop: 20, paddingBottom: 10}}
+              calendarHeaderStyle={{color: 'white'}}
+              calendarColor={'#7743CE'}
+              highlightColor={'#9265DC'}
+              dateNumberStyle={{color: 'white'}}
+              dateNameStyle={{color: 'white'}}
+              borderHighlightColor={'white'}
+              iconLeft={require('./img/left-arrow.png')}
+              iconRight={require('./img/right-arrow.png')}
+              iconContainer={{flex: 0.1}}
+          />
+        </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ReactNativeCalendarStripExample', () => ReactNativeCalendarStripExample);
+AppRegistry.registerComponent('ReactNativeCalendarStripExample', () => ReactNativeCalendarStrip);
--- example/ReactNativeCalendarStripExample/index.ios.js
+++ example/ReactNativeCalendarStripExample/index.ios.js
@@ -18,8 +18,8 @@ class ReactNativeCalendarStrip extends Component {
             <View>
                 <CalendarStrip
                     calendarAnimation={{type: 'sequence', duration: 30}}
-                    selection={'background'}
-                    selectionAnimation={{duration: 300, borderWidth: 1}}
+                    selection={'border'}
+                    selectionAnimation={{duration: 300, borderWidth: 2}}
                     style={{paddingTop: 20, paddingBottom: 10}}
                     calendarHeaderStyle={{color: 'white'}}
                     calendarColor={'#7743CE'}
@@ -29,6 +29,7 @@ class ReactNativeCalendarStrip extends Component {
                     borderHighlightColor={'white'}
                     iconLeft={require('./img/left-arrow.png')}
                     iconRight={require('./img/right-arrow.png')}
+                    iconContainer={{flex: 0.1}}
                 />
             </View>
         );
