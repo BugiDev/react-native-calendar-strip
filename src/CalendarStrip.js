18247eca1dfa8876f50da18741f1fedddcf119de
Peace <pc_5@yahoo.com>
2017-04-20 11:32:33 -0500

    Performance optimizations (see #33)
    Remove method calls in render(): getDatesForWeek, isDateAllowed, getCustomDateStyle.
    Pertinent data is stored in state, updated by props.
    Use View instead of Animated.View when no animation is specified.
    Replace forEach and map with simple for loops.

--- src/CalendarStrip.js
+++ src/CalendarStrip.js
@@ -15,11 +15,6 @@ import CalendarDay from './CalendarDay';
 import moment from 'moment';
 import styles from './Calendar.style.js';
 
-//Just a shallow array of 7 elements
-const arr = [];
-for (let i = 0; i < 7; i++) {
-    arr.push(i);
-}
 /*
  * Class CalendarStrip that is representing the whole calendar strip and contains CalendarDay elements
  *
@@ -91,6 +86,7 @@ export default class CalendarStrip extends Component {
 
     constructor(props) {
         super(props);
+        this.numDaysInWeek = 7;
 
         if(props.locale) {
             if(props.locale.name && props.locale.config) {
@@ -105,6 +101,10 @@ export default class CalendarStrip extends Component {
 
         this.state = {
             startingDate,
+            datesForWeek: [],
+            datesAllowedForWeek: [],
+            datesSelectedForWeek: [],
+            datesCustomStylesForWeek: [],
             selectedDate,
             enableLeftSelector: true,
             enableRightSelector: true,
@@ -114,7 +114,7 @@ export default class CalendarStrip extends Component {
 
         this.componentDidMount = this.componentDidMount.bind(this);
         this.componentWillUpdate = this.componentWillUpdate.bind(this);
-        this.getDatesForWeek = this.getDatesForWeek.bind(this);
+        this.updateWeekData = this.updateWeekData.bind(this);
         this.getPreviousWeek = this.getPreviousWeek.bind(this);
         this.getNextWeek = this.getNextWeek.bind(this);
         this.onDateSelected = this.onDateSelected.bind(this);
@@ -124,9 +124,11 @@ export default class CalendarStrip extends Component {
         this.resetAnimation = this.resetAnimation.bind(this);
     }
 
-    //Animate showing of CalendarDay elements
     componentDidMount() {
-        this.updateWeekView(this.state.startingDate);
+        // Initialize week view
+        let startingDate = this.updateWeekView(); // Force update
+        this.updateWeekData(startingDate);
+        // Animate showing of CalendarDay elements
         this.animate();
     }
 
@@ -134,15 +136,28 @@ export default class CalendarStrip extends Component {
     componentWillReceiveProps(nextProps) {
         if (nextProps.selectedDate !== this.props.selectedDate) {
             let selectedDate = this.setLocale(moment(nextProps.selectedDate));
-            this.updateWeekView(selectedDate);
-            this.setState({
-                selectedDate
+            this.setState({ selectedDate }, () => {
+              // update week view and week data states
+              let startingDate = this.updateWeekView(selectedDate);
+              this.updateWeekData(startingDate, nextProps);
             });
         }
 
+        let _startingDate;
         if (nextProps.startingDate !== this.props.startingDate) {
-            let startingDate = this.setLocale(moment(nextProps.startingDate));
-            this.updateWeekView(startingDate);
+            _startingDate = this.setLocale(moment(nextProps.startingDate));
+            this.updateWeekView(_startingDate, nextProps);
+            this.updateWeekData(_startingDate, nextProps);
+        }
+
+        if (!_startingDate && (
+            nextProps.datesBlacklist !== this.props.datesBlacklist ||
+            nextProps.datesWhitelist !== this.props.datesWhitelist ||
+            nextProps.customDatesStyles !== this.props.customDatesStyles ))
+        {
+            // No need to update week view here
+            _startingDate = this.setLocale(moment(nextProps.startingDate));
+            this.updateWeekData(_startingDate, nextProps);
         }
     }
 
@@ -173,7 +188,7 @@ export default class CalendarStrip extends Component {
 
     //Set startingDate to the previous week
     getPreviousWeek() {
-        const previousWeekStartDate = this.state.startingDate.subtract(1, 'w');
+        const previousWeekStartDate = this.state.startingDate.clone().subtract(1, 'w');
         this.setState({startingDate: previousWeekStartDate});
         if (this.props.onWeekChanged) {
             if(this.props.useIsoWeekday) {
@@ -184,11 +199,12 @@ export default class CalendarStrip extends Component {
             }
         }
         this.updateWeekView(previousWeekStartDate);
+        this.updateWeekData(previousWeekStartDate);
     }
 
     //Set startingDate to the next week
     getNextWeek() {
-        const nextWeekStartDate = this.state.startingDate.add(1, 'w');
+        const nextWeekStartDate = this.state.startingDate.clone().add(1, 'w');
         this.setState({startingDate: nextWeekStartDate});
         if (this.props.onWeekChanged) {
             if(this.props.useIsoWeekday) {
@@ -199,64 +215,78 @@ export default class CalendarStrip extends Component {
             }
         }
         this.updateWeekView(nextWeekStartDate);
+        this.updateWeekData(nextWeekStartDate);
     }
 
     // Set the current visible week to the selectedDate
-    updateWeekView(date, startDate = this.state.startingDate) {
-      let mDate = moment(date).startOf('day');
-      let daysDiff = mDate.diff(moment(startDate).startOf('day'), 'days');
+    // When date param is undefined, an update always occurs (e.g. initialize)
+    updateWeekView(date, props = this.props) {
+      let startingDate = this.state.startingDate.clone().startOf('day');
+      let initialize = date === undefined;
+      let mDate = initialize ? startingDate : moment(date).startOf('day');
+      let daysDiff = mDate.diff(startingDate, 'days');
+      if (daysDiff === 0 && !initialize) {
+        return this.state.startingDate;
+      }
       let addOrSubtract = daysDiff > 0 ? 'add' : 'subtract';
       let adjustWeeks = daysDiff / 7;
       adjustWeeks = adjustWeeks > 0 ? Math.floor(adjustWeeks) : Math.ceil(Math.abs(adjustWeeks));
+      startingDate[addOrSubtract](adjustWeeks, 'w');
 
-      let startingDate = startDate[addOrSubtract](adjustWeeks, 'w');
       let newState = {startingDate};
       let endOfWeekDate;
-      if (this.props.minDate || this.props.maxDate) {
+      if (props.minDate || props.maxDate) {
         endOfWeekDate = startingDate.clone().add(6, 'days');
       }
-      if (this.props.minDate) {
-        newState.enableLeftSelector = !moment(this.props.minDate).isBetween(startingDate, endOfWeekDate, 'day', '[]');
+      if (props.minDate) {
+        newState.enableLeftSelector = !moment(props.minDate).isBetween(startingDate, endOfWeekDate, 'day', '[]');
       }
-      if (this.props.maxDate) {
-        newState.enableRightSelector = !moment(this.props.maxDate).isBetween(startingDate, endOfWeekDate, 'day', '[]');
+      if (props.maxDate) {
+        newState.enableRightSelector = !moment(props.maxDate).isBetween(startingDate, endOfWeekDate, 'day', '[]');
       }
       this.setState(newState);
+      return startingDate;
     }
 
-    //Get dates for the week based on the startingDate
-    //Using isoWeekday so that it will start from Monday
-    getDatesForWeek() {
+    // Get & update week states for the week based on the startingDate
+    updateWeekData(startingDate, props = this.props) {
         const me = this;
-        let dates = [];
-        let startDate = moment(this.state.startingDate);
+        let datesForWeek = [];
+        let datesAllowedForWeek = [];
+        let datesSelectedForWeek = [];
+        let datesCustomStylesForWeek = [];
 
-        arr.forEach((item, index) => {
-            let date;
-            if (me.props.useIsoWeekday) {
-              date = me.setLocale(moment(startDate.isoWeekday(index + 1)));
-            }
-            else {
-              date = me.setLocale(moment(startDate).add(index, 'days'));
-            }
-            dates.push(date);
-        });
-        return dates;
+        for (let i=0; i<this.numDaysInWeek; i++) {
+          let date;
+          if (props.useIsoWeekday) {
+            // isoWeekday starts from Monday
+            date = me.setLocale(startingDate.clone().isoWeekday(i + 1));
+          }
+          else {
+            date = me.setLocale(startingDate.clone().add(i, 'days'));
+          }
+          datesForWeek.push(date);
+          datesAllowedForWeek.push(this.isDateAllowed(date, props));
+          datesSelectedForWeek.push(this.isDateSelected(date));
+          datesCustomStylesForWeek.push(this.getCustomDateStyle(date, props));
+        }
+        this.setState({datesForWeek, datesAllowedForWeek, datesSelectedForWeek, datesCustomStylesForWeek});
+        return {datesForWeek, datesAllowedForWeek, datesSelectedForWeek, datesCustomStylesForWeek};
     }
 
     //Handling press on date/selecting date
-    onDateSelected(date) {
-        this.setState({selectedDate: date});
-        if (this.props.onDateSelected) {
-            this.props.onDateSelected(date);
-        }
+    onDateSelected(selectedDate) {
+        this.setState({ selectedDate },
+          () => this.updateWeekData(this.state.startingDate) );
+
+        this.props.onDateSelected && this.props.onDateSelected(selectedDate);
     }
 
     // Check whether date is allowed
-    isDateAllowed(date) {
+    isDateAllowed(date, props = this.props) {
       // datesBlacklist entries override datesWhitelist
-      if (this.props.datesBlacklist !== undefined ) {
-        for (let disallowed of this.props.datesBlacklist) {
+      if (props.datesBlacklist !== undefined ) {
+        for (let disallowed of props.datesBlacklist) {
           // Blacklist start/end object
           if (disallowed.start && disallowed.end) {
             if (date.isBetween(disallowed.start, disallowed.end, 'day', '[]')) {
@@ -271,12 +301,12 @@ export default class CalendarStrip extends Component {
         }
       }
 
-      if (this.props.datesWhitelist === undefined ) {
+      if (props.datesWhitelist === undefined ) {
         return true;
       }
 
       // Whitelist
-      for (let allowed of this.props.datesWhitelist) {
+      for (let allowed of props.datesWhitelist) {
         // start/end object
         if (allowed.start && allowed.end) {
           if (date.isBetween(allowed.start, allowed.end, 'day', '[]')) {
@@ -316,8 +346,8 @@ export default class CalendarStrip extends Component {
         }
     }
 
-    getCustomDateStyle(date) {
-      for (let customDateStyle of this.props.customDatesStyles) {
+    getCustomDateStyle(date, props = this.props) {
+      for (let customDateStyle of props.customDatesStyles) {
         if (customDateStyle.endDate) {
           // Range
           if (date.isBetween(customDateStyle.startDate, customDateStyle.endDate, 'day', '[]')) {
@@ -336,25 +366,27 @@ export default class CalendarStrip extends Component {
     //Function for reseting animations
     resetAnimation() {
         this.animatedValue = [];
-        arr.forEach((value) => {
-            this.animatedValue[value] = new Animated.Value(0);
-        });
+        for (let i=0; i<this.numDaysInWeek; i++) {
+            this.animatedValue.push(new Animated.Value(0));
+        }
     }
 
     //Function to animate showing the CalendarDay elements.
     //Possible cases for animations are sequence and parallel
     animate() {
         if (this.props.calendarAnimation) {
-            let animations = arr.map((item) => {
-                return Animated.timing(
-                    this.animatedValue[item],
+            let animations = [];
+            for (let i=0; i<this.numDaysInWeek; i++) {
+                animations.push(Animated.timing(
+                    this.animatedValue[i],
                     {
                         toValue: 1,
                         duration: this.props.calendarAnimation.duration,
                         easing: Easing.linear
                     }
-                );
-            });
+                )
+              );
+            }
 
             if (this.props.calendarAnimation.type.toLowerCase() === 'sequence') {
                 Animated.sequence(animations).start();
@@ -370,9 +402,12 @@ export default class CalendarStrip extends Component {
 
     //Function that formats the calendar header
     //It also formats the month section if the week is in between months
-    formatCalendarHeader() {
-        let firstDay = this.getDatesForWeek()[0];
-        let lastDay = this.getDatesForWeek()[this.getDatesForWeek().length - 1];
+    formatCalendarHeader(datesForWeek) {
+        if (!datesForWeek || datesForWeek.length === 0) {
+            return;
+        }
+        let firstDay = datesForWeek[0];
+        let lastDay = datesForWeek[datesForWeek.length - 1];
         let monthFormatting = '';
         //Parsing the month part of the user defined formating
         if ((this.props.calendarHeaderFormat.match(/Mo/g) || []).length > 0) {
@@ -395,43 +430,48 @@ export default class CalendarStrip extends Component {
     }
 
     render() {
-        let opacityAnim = 1;
-        let datesRender = this.getDatesForWeek().map((date, index) => {
-            if (this.props.calendarAnimation) {
-                opacityAnim = this.animatedValue[index];
-            }
-            let enabled = this.isDateAllowed(date);
-            return (
-                <Animated.View key={index} style={{opacity: opacityAnim, flex: 1}}>
-                    <CalendarDay
-                        date={date}
-                        selected={this.isDateSelected(date)}
-                        enabled={enabled}
-                        showDayName={this.props.showDayName}
-                        showDayNumber={this.props.showDayNumber}
-                        onDateSelected={() => { if (enabled) this.onDateSelected(date); } }
-                        calendarColor={this.props.calendarColor}
-                        dateNameStyle={this.props.dateNameStyle}
-                        dateNumberStyle={this.props.dateNumberStyle}
-                        weekendDateNameStyle={this.props.weekendDateNameStyle}
-                        weekendDateNumberStyle={this.props.weekendDateNumberStyle}
-                        highlightDateNameStyle={this.props.highlightDateNameStyle}
-                        highlightDateNumberStyle={this.props.highlightDateNumberStyle}
-                        disabledDateNameStyle={this.props.disabledDateNameStyle}
-                        disabledDateNumberStyle={this.props.disabledDateNumberStyle}
-                        disabledDateOpacity={this.props.disabledDateOpacity}
-                        styleWeekend={this.props.styleWeekend}
-                        daySelectionAnimation={this.props.daySelectionAnimation}
-                        customStyle={this.getCustomDateStyle(date)}
-                    />
-                </Animated.View>
-            );
-        });
+        let datesForWeek = this.state.datesForWeek;
+        let datesRender = [];
+        for (let i=0; i<datesForWeek.length; i++) {
+          let enabled = this.state.datesAllowedForWeek[i];
+          let calendarDay = (
+            <CalendarDay
+                date={datesForWeek[i]}
+                selected={this.state.datesSelectedForWeek[i]}
+                enabled={enabled}
+                showDayName={this.props.showDayName}
+                showDayNumber={this.props.showDayNumber}
+                onDateSelected={() => enabled && this.onDateSelected(datesForWeek[i]) }
+                calendarColor={this.props.calendarColor}
+                dateNameStyle={this.props.dateNameStyle}
+                dateNumberStyle={this.props.dateNumberStyle}
+                weekendDateNameStyle={this.props.weekendDateNameStyle}
+                weekendDateNumberStyle={this.props.weekendDateNumberStyle}
+                highlightDateNameStyle={this.props.highlightDateNameStyle}
+                highlightDateNumberStyle={this.props.highlightDateNumberStyle}
+                disabledDateNameStyle={this.props.disabledDateNameStyle}
+                disabledDateNumberStyle={this.props.disabledDateNumberStyle}
+                disabledDateOpacity={this.props.disabledDateOpacity}
+                styleWeekend={this.props.styleWeekend}
+                daySelectionAnimation={this.props.daySelectionAnimation}
+                customStyle={this.state.datesCustomStylesForWeek[i]}
+            />
+          );
+          datesRender.push( this.props.calendarAnimation ?
+            <Animated.View key={i} style={{opacity: this.animatedValue[i], flex: 1}}>
+              {calendarDay}
+            </Animated.View>
+            :
+            <View key={i} style={{flex: 1}}>
+              {calendarDay}
+            </View>
+          );
+        }
         let leftOpacity = {opacity: this.state.enableLeftSelector ? 1 : 0};
         let rightOpacity = {opacity: this.state.enableRightSelector ? 1 : 0};
         let leftSelector = this.props.leftSelector  || <Image style={[styles.icon, this.props.iconStyle, this.props.iconLeftStyle, leftOpacity]} source={this.props.iconLeft}/>;
         let rightSelector = this.props.rightSelector || <Image style={[styles.icon, this.props.iconStyle, this.props.iconRightStyle, rightOpacity]} source={this.props.iconRight}/>;
-        let calendarHeader = this.props.showMonth && <Text style={[styles.calendarHeader, this.props.calendarHeaderStyle]}>{this.formatCalendarHeader()}</Text>;
+        let calendarHeader = this.props.showMonth && <Text style={[styles.calendarHeader, this.props.calendarHeaderStyle]}>{this.formatCalendarHeader(datesForWeek)}</Text>;
 
         // calendarHeader renders above the dates & left/right selectors if dates are shown.
         // However if dates are hidden, the header shows between the left/right selectors.
