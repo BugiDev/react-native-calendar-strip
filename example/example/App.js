5da5debd1c5801a7dbf1293183f766430c32fadd
Chris Burns <burnsy@outlook.com>
2017-12-21 14:05:38 +0000

    Working New Example

---
+++ example/example/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native Calendar Strip
+ * https://github.com/BugiDev/react-native-calendar-strip
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import CalendarStrip from 'react-native-calendar-strip';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <CalendarStrip
+                    calendarAnimation={{type: 'sequence', duration: 30}}
+                    daySelectionAnimation={{type: 'background', duration: 300, highlightColor: '#9265DC'}}
+                    style={{height:100, paddingTop: 20, paddingBottom: 10}}
+                    calendarHeaderStyle={{color: 'white'}}
+                    calendarColor={'#7743CE'}
+                    dateNumberStyle={{color: 'white'}}
+                    dateNameStyle={{color: 'white'}}
+                    iconLeft={require('./img/left-arrow.png')}
+                    iconRight={require('./img/right-arrow.png')}
+                    iconContainer={{flex: 0.1}}
+                />
+    );
+  }
+}
