1c528dd7dbe806411b0f2efcdf0e219b6c8c6e21
peacechen <github@ghpc.33mail.com>
2020-01-04 01:42:50 -0600

    Update example with customDatesStyles and markedDates.

--- example/example/App.js
+++ example/example/App.js
@@ -6,14 +6,39 @@
 
 import React, { Component } from 'react';
 import CalendarStrip from "react-native-calendar-strip";
+import moment from 'moment';
 
 export default class App extends Component<{}> {
   render() {
+    let customDatesStyles = [];
+    let markedDates = [];
+    let startDate = moment();
+    for (let i=0; i<6; i++) {
+      let _date = startDate.clone().add(i, 'days');
+      customDatesStyles.push({
+        startDate: _date, // Single date since no endDate provided
+        dateNameStyle: {color: 'blue'},
+        dateNumberStyle: {color: 'purple'},
+        // Random color...
+        dateContainerStyle: { backgroundColor: `#${(`#00000${(Math.random() * (1 << 24) | 0).toString(16)}`).slice(-6)}` },
+      });
+      markedDates.push({
+        date: _date,
+        dots: [
+          {
+            key: i,
+            color: 'red',
+            selectedDotColor: 'yellow',
+          },
+        ],
+      });
+    }
+
     return (
       <CalendarStrip
                     calendarAnimation={{type: 'sequence', duration: 30}}
                     daySelectionAnimation={{type: 'background', duration: 300, highlightColor: '#9265DC'}}
-                    style={{height:100, paddingTop: 20, paddingBottom: 10}}
+                    style={{height:200, paddingTop: 20, paddingBottom: 10}}
                     calendarHeaderStyle={{color: 'white'}}
                     calendarColor={'#7743CE'}
                     dateNumberStyle={{color: 'white'}}
@@ -21,6 +46,8 @@ export default class App extends Component<{}> {
                     iconLeft={require('./img/left-arrow.png')}
                     iconRight={require('./img/right-arrow.png')}
                     iconContainer={{flex: 0.1}}
+                    customDatesStyles={customDatesStyles}
+                    markedDates={markedDates}
                 />
     );
   }
