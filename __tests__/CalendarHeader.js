0f200c3fb0f9ca72864ed699875f8b2f6d8401ba
Chandra Setiawan <chsetiaw@microsoft.com>
2020-12-28 01:12:01 -0800

    add unit test for Scroller

--- __tests__/CalendarHeader.js
+++ __tests__/CalendarHeader.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import moment from "moment";
 
 import CalendarHeader from "../src/CalendarHeader";
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 const today = moment();
 
