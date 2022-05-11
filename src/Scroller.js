00b97a1789ec0129ada80de82279e57252beaa6b
Adam Gajzlerowicz <nelf86@gmail.com>
2022-05-12 00:35:09 +0200

    Fix exception when RecyclerListView is not defined (#306)

--- src/Scroller.js
+++ src/Scroller.js
@@ -143,7 +143,7 @@ export default class CalendarScroller extends Component {
 
     for (let i = 0; i < this.state.data.length; i++) {
       if (this.state.data[i].date.isSame(targetDate, "day")) {
-        this.rlv.scrollToIndex(i, true);
+        this.rlv?.scrollToIndex(i, true);
         break;
       }
     }
